cc6ebdcc78c2a603ecc6a99b3228947f8b13b550
andy1138 <andy@hicks.net>
2016-05-16 10:37:09 +0100

    Init

--- README.md
+++ README.md
@@ -1,11 +1,48 @@
-
 # sbt-distributables-plugin-25
 
-[![Build Status](https://travis-ci.org/hmrc/sbt-distributables-plugin-25.svg?branch=master)](https://travis-ci.org/hmrc/sbt-distributables-plugin-25) [ ![Download](https://api.bintray.com/packages/hmrc/sbt-plugin-releases/sbt-distributables-plugin-25/images/download.svg) ](https://bintray.com/hmrc/sbt-plugin-releases/sbt-distributables-plugin-25/_latestVersion)
+[![Join the chat at https://gitter.im/hmrc/sbt-distributables-plugin-25](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/hmrc/sbt-distributables-plugin-25?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge) [![Apache-2.0 license](http://img.shields.io/badge/license-Apache-brightgreen.svg)](http://www.apache.org/licenses/LICENSE-2.0.html) [![Build Status](https://travis-ci.org/hmrc/sbt-distributables-plugin-25.svg)](https://travis-ci.org/hmrc/sbt-distributables-plugin-25) [ ![Download](https://api.bintray.com/packages/hmrc/sbt-plugin-releases/sbt-distributables-plugin-25/images/download.svg) ](https://bintray.com/hmrc/sbt-plugin-releases/sbt-distributables-plugin-25/_latestVersion) [![Stories in Ready](https://badge.waffle.io/hmrc/sbt-distributables-plugin-25.png?label=ready&title=Ready)](https://waffle.io/hmrc/sbt-distributables-plugin-25)
 
-This is a placeholder README.md for a new repository
+An SBT plugin to create then publish .tgz artifacts
 
-### License
+Usage
+-----
 
-This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html").
-    
+In your `project/plugins.sbt` file:
+```
+resolvers += Resolver.url("hmrc-sbt-plugin-releases",
+  url("https://dl.bintray.com/hmrc/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-distributables-plugin-25" % "x.x.x")
+```
+
+where 'x.x.x' is the latest release as advertised above.
+
+In your `project/FrontendBuild.scala` or `project/MicroserviceBuild.scala`
+
+1. Add the line ```.enablePlugins(SbtDistributablesPlugin)``` to enable artifact creation
+2. Add the line ```.settings(SbtDistributablePlugin.publisherSettings)``` to enable artifact publication
+
+Optional settings:
+
+1. Add the line ```.settings(extraFiles := Seq(new File("<PATH-TO-FILE>")))``` to add extra files to the archive (path must be relative)
+
+What it does
+------------
+
+When enabled sbt-distributables-plugin-25 automatically creates and publishes a .tgz artifact. For an artifact `tester` with version `1.0.0` this would result in the following:
+
+```
+tester-1.0.0.tgz
+  .
+    tester-1.0.0
+      bin
+      conf
+      lib
+      share
+      README.md
+    Procfile
+    start-docker.sh
+    system.properties
+```
+
+`system.properties` will specify a `java.runtime.version` derived from the `scalacOptions` setting in SBT. 
---
+++ src/main/scala/uk/gov/hmrc/sbtdistributables/SbtDistributablesPlugin.scala
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.sbtdistributables
+
+import java.io.{File, _}
+import java.lang.System.currentTimeMillis
+import java.util.zip.{ZipEntry, ZipInputStream}
+
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream._
+import org.apache.commons.compress.archivers.tar.{TarArchiveEntry, TarArchiveOutputStream}
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream
+import org.apache.commons.compress.utils.IOUtils._
+import sbt.Keys._
+import sbt._
+
+object SbtDistributablesPlugin extends AutoPlugin {
+
+  override def trigger = allRequirements
+  val logger = ConsoleLogger()
+
+  lazy val distZip = com.typesafe.sbt.SbtNativePackager.NativePackagerKeys.dist
+  lazy val distTgzTask = TaskKey[sbt.File]("dist-tgz", "create tgz distributable")
+  val publishTgz = TaskKey[sbt.File]("publish-tgz", "publish tgz artifact")
+
+  lazy val extraFiles = SettingKey[Seq[File]]("extraFiles", "Extra files to be added to the tgz")
+
+  private val FILE_MODE_775 = 493
+
+  lazy val publishingSettings : Seq[sbt.Setting[_]] = addArtifact(artifact in publishTgz, publishTgz)
+
+  override def projectSettings = Seq(
+    extraFiles := Seq.empty[File],
+    distTgzTask := {
+      createTgz(target.value / "universal", name.value, version.value, javaRuntimeVersion(scalacOptions.value), extraFiles.value)
+    },
+
+    artifact in publishTgz ~= {
+      (art: Artifact) => art.copy(`type` = "zip", extension = "tgz")
+    },
+
+    publishTgz <<= (target, normalizedName, version) map {
+      (targetDir, id, version) => targetDir / "universal" / s"$id-$version.tgz"
+    },
+
+    publishArtifact in(Test, packageDoc) := false,
+    publishArtifact in(Test, packageSrc) := false,
+    publishArtifact in(Test, packageBin) := false,
+    publishArtifact in(Compile, packageDoc) := false,
+    publishArtifact in(Compile, packageSrc) := false,
+    publishArtifact in(Compile, packageBin) := true,
+
+    distTgzTask <<= distTgzTask dependsOn distZip,
+    publishLocal <<= publishLocal dependsOn distTgzTask
+  )
+
+  private def createTgz(targetDir: File, artifactName: String, version: String, javaRuntimeVersion: String, extraFiles: Seq[File]): File = {
+    val externalTgzFiles = extraTgzFiles(javaRuntimeVersion)
+
+    val zip = targetDir / s"$artifactName-$version.zip"
+    val tgz = targetDir / s"$artifactName-$version.tgz"
+    val root = new File(".")
+
+    var inputStream: ZipInputStream = null
+    var outputStream: TarArchiveOutputStream = null
+    try {
+      inputStream = zipInputStream(zip)
+      outputStream = tarArchiveOutputStream(tgz)
+
+      outputStream.putArchiveEntry(new TarArchiveEntry(root))
+      outputStream.closeArchiveEntry()
+
+      addEntries(outputStream, root, extraFiles)
+      addEntries(outputStream, root, externalTgzFiles)
+      copyEntries(inputStream, outputStream, root, artifactName)
+      logger.info(s"Your package is ready in $tgz")
+    } finally {
+      closeQuietly(inputStream)
+      closeQuietly(outputStream)
+    }
+    tgz
+  }
+
+  private def addEntries(outputStream: TarArchiveOutputStream, root: File, extraFiles: Array[(String, String, Option[Int])]) =
+    for (extraFile <- extraFiles) {
+      val bytes = extraFile._2.getBytes("UTF-8")
+      addEntry(outputStream, root, new ByteArrayInputStream(bytes), extraFile._1, bytes.length)
+    }
+
+
+  private def addEntries(outputStream: TarArchiveOutputStream, root: File, extraFiles: Seq[File]) =
+    extraFiles.foreach { extraFile =>
+      val stream = new FileInputStream(extraFile.getAbsoluteFile)
+      addEntry(outputStream, root, stream, extraFile.name, extraFile.getAbsoluteFile.length)
+    }
+
+
+  private def addEntry(outputStream: TarArchiveOutputStream, root: File, stream: InputStream, name: String, size: Long) = {
+    outputStream.putArchiveEntry(tarArchiveEntry(root, name, size, currentTimeMillis(), None))
+    copy(stream, outputStream)
+    outputStream.closeArchiveEntry()
+  }
+
+  private def copyEntries(inputStream: ZipInputStream, outputStream: TarArchiveOutputStream, root: File, artifactName: String) {
+    var inputZipEntry: ZipEntry = inputStream.getNextEntry
+    while (inputZipEntry != null) {
+      outputStream.putArchiveEntry(tarArchiveEntry(root, inputZipEntry.getName, inputZipEntry.getSize, inputZipEntry.getTime, getTarEntryMode(inputZipEntry.getName, artifactName)))
+      copy(inputStream, outputStream)
+      outputStream.closeArchiveEntry()
+      inputZipEntry = inputStream.getNextEntry
+    }
+  }
+
+  private def getTarEntryMode(zipEntryName: String, artifactName: String): Option[Int] = {
+    if (zipEntryName.endsWith(s"/bin/$artifactName")) {
+      Some(FILE_MODE_775)
+    } else {
+      None
+    }
+  }
+
+  private def tarArchiveEntry(root: File, name: String, size: Long, time: Long, mode: Option[Int]): TarArchiveEntry = {
+    val outputTarEntry = new TarArchiveEntry(root / name)
+    outputTarEntry.setSize(size)
+    outputTarEntry.setModTime(time)
+    mode.foreach(m => outputTarEntry.setMode(m))
+    outputTarEntry
+  }
+
+  private def extraTgzFiles(javaRuntimeVersion: String): Array[(String, String, Option[Int])] = {
+    Array(("system.properties", s"java.runtime.version=$javaRuntimeVersion", None))
+  }
+
+  private def javaRuntimeVersion(scalacOptions: Seq[String]): String = {
+    if (scalacOptions.contains("-target:jvm-1.8")) "1.8" else "1.7"
+  }
+
+  private def zipInputStream(zip: File): ZipInputStream = {
+    new ZipInputStream(new BufferedInputStream(new FileInputStream(zip)))
+  }
+
+  private def tarArchiveOutputStream(file: File): TarArchiveOutputStream = {
+    val outputStream = new TarArchiveOutputStream(new GzipCompressorOutputStream(new BufferedOutputStream(new FileOutputStream(file))))
+    outputStream.setLongFileMode(LONGFILE_GNU)
+    outputStream
+  }
+}
