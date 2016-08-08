f428c941e8acff0520be10b5abacedb4c46e8da0
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2016-08-08 14:16:33 +0100

    [PS] Updated to most recent version of sbt-auto-build

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -30,9 +30,6 @@ object PluginBuild extends Build {
       targetJvm := "jvm-1.7",
       organization := "uk.gov.hmrc",
       scalaVersion := "2.10.4",
-      resolvers += Resolver.url(
-        "sbt-plugin-releases",
-        url("https://dl.bintray.com/content/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns),
       libraryDependencies ++= Seq(
         "org.apache.commons" % "commons-compress" % "1.9",
         "org.scalatest" %% "scalatest" % "2.2.4" % "test",
